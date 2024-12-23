517f02683c050b75826be7de0c6dd62b59ed6ecb
Lumina <erik04.nivala22@gmail.com>
2024-12-22 22:18:05 -0600

    Updated cron job and port

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,13 @@ app.use(express.json());
 app.use(Authentication);
 app.use(APIRouter);
 
-// cron.schedule("*/5 * * * *", async () => {
-//   const start = performance.now();
-//   console.log(`Running scheduled API updates! at time: ${Date.now.toString()}`);
-//   await getPricingDataForAllTradableItems();
-//   const time = performance.now() - start;
-//   console.log(`Finished scheduled API updates! It took: ${time} milliseconds`);
-// });
+cron.schedule("*/5 * * * *", async () => {
+  const start = performance.now();
+  console.log(`Running scheduled API updates! at time: ${Date.now.toString()}`);
+  await getPricingDataForAllTradableItems();
+  const time = performance.now() - start;
+  console.log(`Finished scheduled API updates! It took: ${time} milliseconds`);
+});
 
 app.listen(port, () => {
   console.log(`Started Tyria Tracker backend on port ${port}`);
